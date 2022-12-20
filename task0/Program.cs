caff1ccce09cb6697998b960e34dec8be6e1cc22
Beliatskaya <beliatskaya1003@gmail.com>
2022-12-20 23:01:06 +0300

    DZ

---
+++ task0/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+    {
+        Console.WriteLine("Итог :");
+        Console.WriteLine("a = "+ a);
+        Console.WriteLine("b = "+ b);
+    }
+else 
+    
+        Console.WriteLine("Повторите попытку.");
+    
---
+++ task3/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 1)
+    {
+        Console.WriteLine("Понедельник");
+    }
+else if (a == 2)
+    {
+        Console.WriteLine("Вторник");
+    }
+else if (a == 3)
+    {
+        Console.WriteLine("Среда");
+    }
+else if (a == 4)
+    {
+        Console.WriteLine("Четверг");
+    }
+else if (a == 5)
+    {
+        Console.WriteLine("Пятница");
+    }
+else if (a == 6)
+    {
+        Console.WriteLine("Суббота");
+    }
+else if (a == 7)
+    {
+        Console.WriteLine("Воскресенье");
+    }
+else
+    {
+        Console.WriteLine("Ошибка");
+    }
+
+
+//Другой вариант решения
+
+//switch(DayNumber)
+//{ 
+//  case 2:   
+//      Console.WriteLine("Вторник");
+//      break;
+//  case 3: и тд.
+//...
+// Конец программы выглядит так:
+// default(когда ничего не подходит):
+//      Console.WriteLine("Такого дня не существует");
+//      break;
+//}
